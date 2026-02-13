87eb0219bf2ef8263858a291fe52544d38e45098
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-02-12 22:21:39 -0500

    debut du script

---
+++ index.js
@@ -0,0 +1,4 @@
+//Declaration et initialisation de nos variables
+let recipe = document.getElementById("recipe");
+
+const URL = `https://api.spoonacular.com/recipes/random?number=10&apiKey=275d58779ccf4e22af03e792e8819fff`;
