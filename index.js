ae89f81c63195375d79a14eb76b36c337518770d
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-02-14 14:57:52 -0500

    mise a jour du code

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 //Declaration et initialisation de nos variables
-let recipe = document.getElementById("recipeListes");
+let recipeListes = document.getElementById("recipeListes");
 
 const URL = `https://api.spoonacular.com/recipes/random?number=10&apiKey=275d58779ccf4e22af03e792e8819fff`;
 
 //La fonction du traitement des donnees
+function displayRecipes(recipes) {
+	recipeListes.textContent = "";
+	recipes.forEach((recipe) => {
+		//creons nos items pour pouvoir leur ajouter a notre listes d'items.
+		let itemsListe = document.createElement("li");
+		itemsListe.classList.add("recipe-items");
 
+		let itemsImg = document.createElement("img");
+		itemsImg.src = recipe.image;
+		itemsImg.alt = "recipe image";
+
+		let itemsTitle = document.createElement("h2");
+		itemsTitle.textContent = recipe.title;
+
+		let recipeIngredients = document.createElement("p");
+		recipeIngredients.innerHTML = `<strong>Ingredients:</strong> ${recipe.extendedIngredients.map((ingredient) => ingredient.original).join(", ")}`;
+	});
+}
 
 //La fonction de recuperation des donnees a travers l'API
 async function getRecipe() {
